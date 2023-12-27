c20345d5759025a1a243af4b7ed0f4fa3b4ff1f0
Bala Viswanadh <balaviswanadh21@gmail.com>
2023-12-27 10:39:08 +0530

    Adding Path Variable

--- main.py
+++ main.py
@@ -5,3 +5,7 @@ app = FastAPI()
 @app.get("/")
 async def home():
     return {"message" : "This is Home Page"}
+
+@app.get("/home/{feature_name}")
+async def get_feature(feature_name: str):
+    return {'feature' : feature_name}
