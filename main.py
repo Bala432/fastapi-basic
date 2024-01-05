7efdda71f638188e43835594c56dc8eb84102767
Bala Viswanadh <balaviswanadh21@gmail.com>
2024-01-05 19:32:48 +0530

    Basic Params Test

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Path
 from models import Item
+from typing import Annotated
 
 app = FastAPI()
 
@@ -7,3 +8,13 @@ app = FastAPI()
 async def root():
     return {'Home' : ' This is Home Page'}
 
+@app.post('/items')
+async def post(item: Item):
+    if item.tax is not None:
+        item.price = item.price + (item.price * item.tax) / 100
+    
+    return {"Item" : item}
+
+@app.get('/items/{item_id}')
+async def get(item_id: Annotated[int,Path( title = "Get Item ID", ge=3)]):
+    return { 'Item' : item_id}
