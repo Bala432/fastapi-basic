c5139f4ff4ff8077b567afe771bc395b1366a5db
Bala Viswanadh <balaviswanadh21@gmail.com>
2024-01-05 18:48:30 +0530

    Updating Model to Item

--- main.py
+++ main.py
@@ -1,37 +1,9 @@
-from uuid import UUID, uuid4
 from fastapi import FastAPI
-from models import User, Gender, Role
-from typing import List
+from models import Item
 
 app = FastAPI()
 
-db: List[User] = [
-    User(id=uuid4(),first_name='Thomas',last_name='Shelby',gender=Gender.male,roles=[Role.admin,Role.user]),
-    User(id=uuid4(),first_name='Grace',last_name='Burgess',gender=Gender.female,roles=[Role.user])
-]   
+@app.get('/')
+async def root():
+    return {'Home' : ' This is Home Page'}
 
-@app.get("/")
-async def home():
-    return {"message" : "This is Home Page"}
-
-@app.get("/home/{feature_name}")
-async def get_feature(feature_name: str):
-    return {'feature' : feature_name}
-
-@app.get('/users/list')
-async def get_users_list():
-    return db
-
-@app.post('/create-user')
-async def create_user(user: User):
-    db.append(user)
-    return {'users' : db}
-
-@app.delete('/user/{user_id}/delete')
-async def delete_user(user_id: UUID):
-    for user in db:
-        if user.id == user_id:
-            db.remove(user)
-            return {'Removed UserID' : user_id}
-        
-    
--- models.py
+++ models.py
@@ -1,21 +1,7 @@
-from enum import Enum
-from typing import List, Optional
-from uuid import UUID, uuid4
 from pydantic import BaseModel
 
-class Gender(str, Enum):
-    male = "male"
-    female = "female"
-    
-class Role(str, Enum):
-    admin = "admin"
-    user = "user"
-    student = "student"
-    
-class User(BaseModel):
-    id: Optional[UUID] = uuid4()
-    first_name: str
-    last_name: str
-    gender: Gender
-    roles: List[Role]
-    
+class Item(BaseModel):
+    name : str
+    description : str | None = None
+    price : float
+    tax : float | None = None    
