6dcccd1e2032e098632fc26dcd20abf14b3d7841
Bala Viswanadh <balaviswanadh21@gmail.com>
2024-01-07 12:14:09 +0530

    Adding Field Validations to Model

--- models.py
+++ models.py
@@ -1,7 +1,12 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, Field, HttpUrl
 
+class Image(BaseModel):
+    url : HttpUrl = Field(title="URL of Image")
+    name : str = Field(title="Name of the Image", min_length=5, max_length=20)
+    
 class Item(BaseModel):
-    name : str
-    description : str | None = None
-    price : float
-    tax : float | None = None    
+    name : str = Field(description="Name of Item", min_length=5, max_length=50)
+    description : str | None = Field(default=None,description="Description of the Item", max_length=300)
+    price : float = Field(gt=0, description="Price of the Item")
+    tax : float | None = Field(default=None, ge=0, description="Applied Tax for the Item")
+    image : Image | None = None
